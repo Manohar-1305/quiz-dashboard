8dcdf3fe64d90989a73c113d1d5d99ae4891a397
Manohar-1305 <maddy98989@gmail.com>
2025-03-27 21:50:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,66 +2,57 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-
-data "aws_availability_zones" "available" {}
-
-
-resource "aws_vpc" "hosting_vpc" {
+resource "aws_vpc" "prod_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Hosting-VPC"
+    Name = "prod-VPC"
   }
 }
 
-# Create a Public Subnet in the first available AZ
-resource "aws_subnet" "hosting_subnet" {
-  vpc_id                  = aws_vpc.hosting_vpc.id
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "prod_subnet" {
+  vpc_id                  = aws_vpc.prod_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0] # Picks the first available AZ
+  availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name = "Hosting-Public-1"
+    Name = "prod-Public-1"
   }
 }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "hosting_igw" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_internet_gateway" "prod_igw" {
+  vpc_id = aws_vpc.prod_vpc.id
 
   tags = {
-    Name = "Hosting-IGW"
+    Name = "prod-IGW"
   }
 }
 
-# Create a Route Table
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_route_table" "prod_rt" {
+  vpc_id = aws_vpc.prod_vpc.id
 
   tags = {
-    Name = "Hosting-Public-RT"
+    Name = "prod-Public-RT"
   }
 }
 
-# Add a Route for Internet Access
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public_rt.id
+  route_table_id         = aws_route_table.prod_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.hosting_igw.id
+  gateway_id             = aws_internet_gateway.prod_igw.id
 }
 
-# Associate the Route Table with the Public Subnet
-resource "aws_route_table_association" "public_subnet_assoc" {
-  subnet_id      = aws_subnet.hosting_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "prod_subnet_assoc" {
+  subnet_id      = aws_subnet.prod_subnet.id
+  route_table_id = aws_route_table.prod_rt.id
 }
 
-# Create a Security Group (Open to Everywhere)
-resource "aws_security_group" "open_sg" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_security_group" "prod_sg" {
+  vpc_id = aws_vpc.prod_vpc.id
 
-  # Allow all inbound traffic (Not recommended for production)
   ingress {
     from_port   = 0
     to_port     = 0
@@ -69,7 +60,6 @@ resource "aws_security_group" "open_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -78,7 +68,7 @@ resource "aws_security_group" "open_sg" {
   }
 
   tags = {
-    Name = "Open-SG"
+    Name = "prod-SG"
   }
 }
 
@@ -86,18 +76,19 @@ resource "aws_instance" "mysql-server" {
   ami                    = "ami-03bb6d83c60fc5f7c"
   instance_type          = "t2.medium"
   key_name               = "testing-dev-1"
-  subnet_id              = aws_subnet.hosting_subnet.id
-  vpc_security_group_ids = [aws_security_group.open_sg.id]
-
+  subnet_id              = aws_subnet.prod_subnet.id
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
+  user_data              = file("user_data.sh")
 
   # Set root volume size to 25GB
   root_block_device {
-    volume_size           = 25    # Size in GB
-    volume_type           = "gp3" # General Purpose SSD (default is gp2)
-    delete_on_termination = true  # Deletes volume when instance is terminated
+    volume_size           = 25
+    volume_type           = "gp3"
+    delete_on_termination = true
   }
+  
   tags = {
-    Name = "Jmysql-server"
+    Name = "mysql-server"
   }
 }
 
@@ -105,22 +96,19 @@ resource "aws_instance" "app-server" {
   ami                    = "ami-03bb6d83c60fc5f7c"
   instance_type          = "t2.medium"
   key_name               = "testing-dev-1"
-  subnet_id              = aws_subnet.hosting_subnet.id
-  vpc_security_group_ids = [aws_security_group.open_sg.id]
-  user_data              = file("jenkins-slave.sh")
+  subnet_id              = aws_subnet.prod_subnet.id
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
 
   tags = {
     Name = "app-server"
   }
 }
 
-
 # Outputs
-output "Jenkins_master_public_ip" {
+output "mysql_server_public_ip" {
   value = aws_instance.mysql-server.public_ip
 }
 
-output "Jenkins_slave_public_ip" {
+output "app_server_public_ip" {
   value = aws_instance.app-server.public_ip
 }
-
