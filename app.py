8c541e47fd4d00256816720b420ad39821113fbf
Manohar-1305 <maddy98989@gmail.com>
2025-03-27 20:03:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_admin()  # Ensures admin exists
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
